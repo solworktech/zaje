713b605ba16d3dcf9568ae85ae2cc7de9924bf7f
Jesse Portnoy <jesse@packman.io>
2025-02-19 20:04:58 +0000

    postinst script to let the user know they should source zaje.sh so it affects the current session.
    (Sourcing from postinst won't affect the session the package was
    installed from)

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,2 +1,6 @@
-echo "run '. /etc/profile.d/zaje.sh' to define the Zaje aliases for your current session. 
-Will be sourced automatically for future sessions."
+cat << EOF
+##############################################################################################
+# run '. /etc/profile.d/zaje.sh' to define the Zaje aliases for your current session.        #
+# They will be sourced automatically in future sessions.                                     #
+##############################################################################################
+EOF
